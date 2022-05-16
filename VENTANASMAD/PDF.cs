afc227610675d98cb072602919b603a8bbce4cf7
JuanIram02 <93635490+JuanIram02@users.noreply.github.com>
2022-05-16 13:11:14 -0500

    Pdf

---
+++ VENTANASMAD/PDF.cs
@@ -0,0 +1,23 @@
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VENTANASMAD
+{
+    class PDF
+    {
+        void generaPDF(string numeroNomina)
+        {
+            Document pdfDocument = new Document();
+            Page page1 = pdfDocument.Pages.Add();
+            BackgroundArtifact bg = new BackgroundArtifact();
+            bg.BackgroundImage = File.OpenRead("CFE.png");
+        }
+       
+    }
+}
